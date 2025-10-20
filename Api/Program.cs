69d648a675536dcc6a7477891261a3e49acff588
Agustin S. <asantinaque@protonmail.com>
2025-10-20 03:33:30 -0300

    fix: mejoras en conexion db para mas robustez

--- Api/Program.cs
+++ Api/Program.cs
@@ -4,7 +4,9 @@ using Api.Mapping;
 using Api.Mapping.Interface;
 using Api.Service;
 using Api.Service.Interface;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +15,14 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<NoteContext>(opt => opt.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
 
-builder.Services.AddHealthChecks().AddDbContextCheck<NoteContext>("db");;
+
+builder.Services.AddHealthChecks().AddDbContextCheck<NoteContext>("db"); ;
+
+builder.Services.AddHealthChecks()
+    .AddCheck("self", () => HealthCheckResult.Healthy())
+    .AddDbContextCheck<NoteContext>("db");
+
+
 
 builder.Services.AddScoped<INoteMapper, NoteMappers>();
 builder.Services.AddScoped<INoteService, NoteService>();
@@ -27,7 +36,12 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>(
 
 var app = builder.Build();
 
-app.MapHealthChecks("/health");
+app.MapHealthChecks("/health-db");
+
+app.MapHealthChecks("/health", new HealthCheckOptions {
+    Predicate = r => r.Name == "self"
+});
+
 
 if (args.Contains("--migrate-only"))
 {
@@ -35,6 +49,21 @@ if (args.Contains("--migrate-only"))
     var db = scope.ServiceProvider.GetRequiredService<NoteContext>();
     await db.Database.MigrateAsync();
     return;
+}
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<NoteContext>();
+    try
+    {
+        var can = await db.Database.CanConnectAsync();
+        app.Logger.LogInformation("DB CanConnect: {can}", can);
+    }
+    catch (Exception ex)
+    {
+        app.Logger.LogError(ex, "DB connection failed");
+    }
 }
 
 
